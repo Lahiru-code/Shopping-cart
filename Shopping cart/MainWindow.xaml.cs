8058be88bbba17412c64b6ccfae5ff74c5e1e42c
Lahiru-code <lahirulakshanjayalath66@gmail.com>
2025-03-06 12:54:15 +0530

    Add project files.

---
+++ Shopping cart/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+using Shopping_cart.UserControls;
+
+
+namespace Shopping_cart
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+         
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+
+                this.DragMove();
+            }
+        }
+
+        
+
+    }
+    }
---
+++ cart/cart/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Shopping_cart.UserControls;
+
+
+namespace Shopping_cart
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+
+                this.DragMove();
+            }
+        }
+
+
+    }
+}
